c774d3efeba3ab6fb3cba7a0d9c36ad0d55e8575
maxime-ui <maxchabaud01@gmail.com>
2022-06-09 13:16:50 +0200

    start du dev du roting de l'application

--- ng-app/src/app/app-routing.module.ts
+++ ng-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './component/pages/accueil/accueil.component';
+import { StreamersComponent } from './component/pages/streamers/streamers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'acceuil', component: AccueilComponent },
+  { path: 'acceuil/streamers', component: StreamersComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
